b78d8364a506b6f753f3d86b37c0d058c52e15cd
Priti-Dawande <kulkarni.prity@gmail.com>
2025-01-06 13:25:41 +0530

    Update ddl.sql
    how to Create,alter(add,modify),truncate,delete Table with practical

--- ddl.sql
+++ ddl.sql
@@ -1,6 +1,8 @@
 --DDL  Commands
+--how to Create,alter(add,modify),truncate,delete Table
 create table exemp (eid number(4),enm varchar2(30),edoj date,esal number(5),desg varchar2(30));
 alter table exemp add(email varchar2(15));
+alter table exemp modify(email varchar2(40));
 create table newexemp as select * from exemp;
 desc newexemp;
 truncate table newexemp;
