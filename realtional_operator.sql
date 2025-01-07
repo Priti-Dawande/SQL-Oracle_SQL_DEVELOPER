05790363a429066336417550f09218b2a8cb189a
Priti-Dawande <kulkarni.prity@gmail.com>
2025-01-07 14:11:27 +0530

    Relational operator

---
+++ realtional_operator.sql
@@ -0,0 +1,7 @@
+--Realtional or Comparison Operator
+select eid,enm,esal as " greator than " from exemp where esal > 50000;
+select eid,enm,esal as " less than " from exemp where esal < 50000;
+select eid,enm,esal as " greator than equals to" from exemp where esal>=50000;
+select eid,enm,esal as " less than equals to " from exemp where esal<=50000;
+select eid,enm,esal as " equals to " from exemp where esal=20000 ;
+select eid,enm,esal as " not equals to " from exemp where esal<>20000 ;
