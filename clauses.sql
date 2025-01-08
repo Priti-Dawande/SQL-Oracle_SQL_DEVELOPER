2a0394140fb932dc17958f16bbaa2ce2cd821490
Priti-Dawande <kulkarni.prity@gmail.com>
2025-01-08 13:23:50 +0530

    Clause (Where, Group by, Having)

---
+++ clauses.sql
@@ -0,0 +1,15 @@
+--clauses 
+--select 
+select * from exemp;
+--where clause
+select * from exemp where enm='prisha' and eid=5;
+--group by clause
+select count(eid),email,enm from exemp group by enm,email;
+--without function
+select email,enm from exemp group by email,enm;
+--having clause
+select * from exemp order by eid;
+--by default its ascending
+select * from exemp order by eid asc;
+--having clause by decending 
+select * from exemp order by eid desc;
